88eb47fc9e834dabf13217b1f7feb05aa515e965
zakumann <samuel742able@gmail.com>
2024-07-17 13:00:39 +0900

    StopDriving

---
+++ Source/DesertRacer/Obstacle.cpp
@@ -0,0 +1,42 @@
+#include "Obstacle.h"
+
+#include "PlayerCharacter.h"
+
+AObstacle::AObstacle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
+	SetRootComponent(CapsuleComp);
+
+	ObstacleSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("ObstacleSprite"));
+	ObstacleSprite->SetupAttachment(RootComponent);
+}
+
+void AObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CapsuleComp->OnComponentBeginOverlap.AddDynamic(this, &AObstacle::OverlapBegin);
+}
+
+void AObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AObstacle::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 otherBodyIndex, bool FromSweep, const FHitResult& SweepResult)
+{
+	APlayerCharacter* Player = Cast<APlayerCharacter>(OtherActor);
+	if (Player)
+	{
+		if (Player->CanMove)
+		{
+			Player->CanMove = false;
+
+
+		}
+	}
+}
+
---
+++ Source/DesertRacer/Obstacle.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/CapsuleComponent.h"
+#include "PaperSpriteComponent.h"
+
+#include "Obstacle.generated.h"
+
+UCLASS()
+class DESERTRACER_API AObstacle : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UPaperSpriteComponent* ObstacleSprite;
+
+	AObstacle();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 otherBodyIndex, bool FromSweep, const FHitResult& SweepResult);
+};
